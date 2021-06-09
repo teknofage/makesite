9e10af71bea13425880d4ee03bb0b157b4ee2660
teknofage <teknofage@gmail.com>
2021-06-09 15:02:50 -0700

    Edited Lne 4 of Readme, read in file contents

--- makesite.go
+++ makesite.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
 func main() {
+	fileContents, err := ioutil.ReadFile("first-post.txt")
+	if err != nil {
+		// A common use of `panic` is to abort if a function returns an error
+        // value that we don’t know how to (or want to) handle. This example
+        // panics if we get an unexpected error when creating a new file.
+        panic(err)
+	}
 	fmt.Println("Hello, world!")
 }
