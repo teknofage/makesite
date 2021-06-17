bba1748cda2d9a12c0b2f80f773f4f35793a0416
teknofage <teknofage@gmail.com>
2021-06-16 18:33:17 -0700

    created new flag

--- makesite.go
+++ makesite.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"text/template"
+	"flag"
+	"strings"
 )
 type entry struct {
 	Name string
@@ -18,7 +20,18 @@ type ToDo struct {
 
 
 func main() {
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+	// defining flag
+	var filename string
+
+	//define the flag, using the pointer to filename variable
+	flag.StringVar(&filename, "file", "", "Text file name")
+	flag.Parse()
+	if filename == "" {
+		fmt.Println("Wubbalubbadubdub!")
+		return 
+	}
+	
+	fileContents, err := ioutil.ReadFile(filename)
 	if err != nil {
 		// A common use of `panic` is to abort if a function returns an error
         // value that we don’t know how to (or want to) handle. This example
@@ -26,10 +39,14 @@ func main() {
         panic(err)
 	}
 
-	f, err := os.Create("first-post.html")
+		// create file and give it the name of the read txt file, 
+		// and split the txt filename at the . and give it a html extension
+	f, err := os.Create(strings.SplitN(filename, ".", 2)[0] + ".html")
     if err != nil {
         panic(err)
     }
+
+
 
 	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
         err = t.Execute(f, string(fileContents))
