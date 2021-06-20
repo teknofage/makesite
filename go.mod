88c4503b45d1361e893921c923bdd662c66d1c7a
teknofage <teknofage@gmail.com>
2021-06-19 22:03:33 -0700

    integrated Google Translate but Google Clo is not letting me connect to their services so I can't get teh API working

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/teknofage/makesite // Namespace
 
 go 1.16 //version of go used
+
+require (
+	cloud.google.com/go v0.84.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/oauth2 v0.0.0-20210615190721-d04028783cf1 // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	google.golang.org/genproto v0.0.0-20210617175327-b9e0b3197ced // indirect
+)
--- makesite.go
+++ makesite.go
@@ -8,6 +8,11 @@ import (
 	"text/template"
 	"flag"
 	"strings"
+	"context"
+
+	translate "cloud.google.com/go/translate/apiv3"
+	translatepb "google.golang.org/genproto/googleapis/cloud/translate/v3"
+
 )
 // Holds the title and body from the text file: update for stretch challenge
 type Blog struct {
@@ -68,4 +73,39 @@ func directoryManipulation(directory string) {
 			fileManipulation(file.Name())
 		}
 	}
+}
+
+// translateText translates input text and returns translated text.
+func translateText(text string) error {
+	projectID := "the-ridge-317404"
+	sourceLang := "en-US"
+	targetLang := "fr"
+	// text := "Text you wish to translate"
+
+	ctx := context.Background()
+	client, err := translate.NewTranslationClient(ctx)
+	if err != nil {
+			return fmt.Errorf("NewTranslationClient: %v", err)
+	}
+	defer client.Close()
+
+	req := &translatepb.TranslateTextRequest{
+			Parent:             fmt.Sprintf("projects/%s/locations/global", projectID),
+			SourceLanguageCode: sourceLang,
+			TargetLanguageCode: targetLang,
+			MimeType:           "text/plain", // Mime types: "text/plain", "text/html"
+			Contents:           []string{text},
+	}
+
+	resp, err := client.TranslateText(ctx, req)
+	if err != nil {
+			return fmt.Errorf("TranslateText: %v", err)
+	}
+
+	// Display the translation for each input text provided
+	for _, translation := range resp.GetTranslations() {
+			fmt.Fprintf(w, "Translated text: %v\n", translation.GetTranslatedText())
+	}
+
+	return nil
 }
